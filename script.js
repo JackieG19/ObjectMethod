7499699db999c02ac81e9f18774bf7e198741dc9
Jackie <jacgut19@gmail.com>
2017-11-13 18:47:51 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// Nov.13
+
+var school = {
+    "name": "CareerDevs",
+    twitter: "@CareerDevs",
+    classes: [
+        {
+            name: "Fall 2017 Morning Intro",
+            time: "Morning",
+            type: "Intro",
+            year: 2017,
+            semester: 2,
+            students: [
+                {
+                    name: "Dara Hoy",
+                    grade: 9
+                },
+                {
+                    name: "Lauren George",
+                    grade: 8
+                }
+            ]
+        },
+        {
+            name: "Fall 2017 Evening Intro",
+            time: "Evening",
+            type: "Intermediate",
+            year: 2017,
+            semester: 2,
+            students: [
+                {
+                    name: "Josh Bristol",
+                    grade: 10
+                },
+                {
+                    name: "Matt Curcio",
+                    grade: 7,
+                    gender: "male"
+                }
+            ]
+        }
+    ],
+    
+    addStudent: function(classNum, studentName, studentGrade) {
+        this.classes[classNum].students.push({
+            name: studentName,
+            grade: studentGrade
+        });
+    },
+    
+    getAvgGrade: function(classNum) {
+        // grab the students array from the class number given.
+        var theStudents = this.classes[classNum].students //fill out dot notation
+        var total = 0
+        for(var i = 0; i < theStudents.length; i++) {
+            total += theStudents[i].grade
+        }
+        return total / theStudents.length;
+    }
+}
+
+console.log(school.getAvgGrade(0))
+// terminal - node script.js 
+8.5
